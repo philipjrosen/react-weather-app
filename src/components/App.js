9892917a1d9679ea92c1c4e349817f977e57597e
Philip Rosen <philipjrosen@gmail.com>
2017-05-19 10:57:06 +0300

    Making an Input Component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import ZipForm from './ZipForm';
 
 class App extends React.Component {
   render() {
     return (
       <div className='app'>
-        <p>Scotch School rocks!</p>
+         <ZipForm />
       </div>
     );
   }
